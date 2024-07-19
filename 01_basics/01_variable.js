3547d6f0b60f67b32a009a62f5fad6166f6845d9
Siddhant avadhya <94733231+SiddhantAvadhya@users.noreply.github.com>
2024-07-19 13:47:05 +0000

    commit this

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -3,4 +3,10 @@ let accountEmail = "sidd@google.com"
 var accountPassword = "2345"
 accountCity = "ahmedabad"
 
+let accountState;
+
+accountEmail = "moon@google.com"
+accountPassword = "32312"
+accountCity = "jaipur"
+
 console.table([accountId,accountEmail,accountPassword,accountCity])
