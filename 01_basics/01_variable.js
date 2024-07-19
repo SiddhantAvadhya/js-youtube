f9c089242d1b7e74d5c1988ac2af4615f66949f4
Siddhant avadhya <94733231+SiddhantAvadhya@users.noreply.github.com>
2024-07-19 10:16:23 +0530

    Create 01_variable.js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 176235
+let accountEmail = "sidd@google.com"
+var accountPassword = "2345"
+accountCity = "ahmedabad"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
