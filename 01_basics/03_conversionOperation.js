3be28d546c61b16b77a17f2dcc44e0d3aa649a1c
Siddhant avadhya <94733231+SiddhantAvadhya@users.noreply.github.com>
2024-07-19 14:51:50 +0000

    conv

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+//let score = "33ab"
+//let score = false
+//let score = undefined
+let score = "sidd"
+
+console.log(typeof score)
+
+let letnumber = Number(score)
+console.log(typeof letnumber)
+console.log(letnumber)
+
+//number
+//"33" => 33
+//"sidd" => NaN
+//false => 0
+//undefined => NaN
+
+let num = 1
+let letbool = Boolean(num)
+console.log(letbool);
+
+//boolean
+//1 => true
+//0 => false
+//"" => false
+//"sidd" => true 
