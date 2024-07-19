b9a15665cf58dc5f2523d1ccddddcee61097219b
Siddhant avadhya <94733231+SiddhantAvadhya@users.noreply.github.com>
2024-07-19 14:20:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//number
+//string
+//boolean
+//bigint
+//null
+//undefined
+//symbol
+
+let name = "siddhant"
+let age = 23
+let rank = false
+
+console.log(typeof rank);
+console.log(3+3);
