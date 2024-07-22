53e5f53c91876a5e4a51fdeaae6b2ca1e085a595
Siddhant avadhya <94733231+SiddhantAvadhya@users.noreply.github.com>
2024-07-22 13:40:41 +0000

    hey

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 //let score = "33ab"
 //let score = false
 //let score = undefined
-let score = "sidd"
+// let score = "sidd"
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let letnumber = Number(score)
-console.log(typeof letnumber)
-console.log(letnumber)
+// let letnumber = Number(score)
+// console.log(typeof letnumber)
+// console.log(letnumber)
 
 //number
 //"33" => 33
@@ -15,12 +15,43 @@ console.log(letnumber)
 //false => 0
 //undefined => NaN
 
-let num = 1
-let letbool = Boolean(num)
-console.log(letbool);
+// let num = 1
+// let letbool = Boolean(num)
+// console.log(letbool);
 
 //boolean
 //1 => true
 //0 => false
 //"" => false
 //"sidd" => true 
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1 = "Hello"
+// let str2 = " Siddhant"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
